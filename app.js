56924f6ba9da114df614c225ffe7e61f3239ebc9
McDaddy <McDaddychen@126.com>
2013-08-18 11:09:05 +0800

    test commit

--- app.js
+++ app.js
@@ -8,8 +8,7 @@ var verbose = require('debug')('webot-example:verbose');
 var app = express();
 
 // 实际使用时，这里填写你在微信公共平台后台填写的 token
-var wx_token = process.env.WX_TOKEN || 'keyboardcat123';
-var wx_token2 = process.env.WX_TOKEN_2 || 'weixinToken2';
+var wx_token = process.env.WX_TOKEN || 'cwt123456';
 
 // remove this test code in production environment
 try {
@@ -25,17 +24,6 @@ app.use(express.session({ secret: 'abced111', store: new express.session.MemoryS
 
 // 启动机器人, 接管 web 服务请求，默认会监听根目录请求
 webot.watch(app, { token: wx_token, path: '/wechat' });
-
-// 若省略 path 参数，会监听到根目录
-// webot.watch(app, { token: wx_token, });
-
-// 可以建立多个实例，并监听到不同 path ，
-// 后面指定的 path 不可为前面实例的子目录
-var webot2 = new webot.Webot();
-
-webot2.set('hello', 'hi.');
-
-webot2.watch(app, { token: wx_token2, path: '/wechat_2' });
 
 // 载入路由规则
 require('./rules')(webot);
